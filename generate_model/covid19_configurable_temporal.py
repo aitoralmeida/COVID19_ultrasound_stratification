2704031a21a92ba68e23ca8560c4b4e46ab9dc81
UnaiBermejo <unaibermejofdez@opendeusto.es>
2020-12-02 14:34:00 +0000

    Added CNN + LSTM model V1 for video classification

--- generate_model/covid19_configurable_temporal.py
+++ generate_model/covid19_configurable_temporal.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.random import set_seed
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, TimeDistributed, LSTM, Input, Flatten
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.mobilenet import MobileNet
 from tensorflow.keras.applications.mobilenet import preprocess_input as preprocess_input_v1
@@ -17,36 +17,44 @@ from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input as preprocess_input_v2
 from tensorflow.keras.applications.efficientnet import *
 
-#TODO add to args
-TEST_SET_PATIENTS = ['04_','09_','18_','21_','27_','36_','38_','41_','51_','55_','59_','60_']
-
-def create_sets(path, positive, negative, model_name, model_version, model, train_test_divide):
+def create_sets(path, positive, negative, model_name, model_version, IMG_SIZE, MAX_FRAMES, CHANNELS, train_test_divide):
   files_covid= os.listdir(path)
   total_files = len(files_covid)
   print ('Total files in disk:', total_files)
-  
-  #randomize the files
-  shuffle(files_covid)
+
+  train_patients = []
+  val_patients = []
+
+  frames_by_patient = {}
+  for filename in files_covid:
+    patient_number = int(filename.partition('_')[0])
+    if patient_number not in frames_by_patient.keys():
+      frames_by_patient[patient_number] = list()
+      frames_by_patient[patient_number].append(filename)
+    else:
+      frames_by_patient[patient_number].append(filename)
+
+  print('Total patients: ' + str(len(frames_by_patient.keys())))
 
   #find positive and negative files
   print('*'*10)
-  print('Separating posititive and negative files...')
+  print('Separating posititive and negative patients...')
   print('Positive token:', positive)
   print('Negative token', negative)
-  positive_files = []
-  negative_files = []
-  for name in files_covid:
-    if negative in name:
-      negative_files.append(name) 
-    elif positive in name:
-      positive_files.append(name) 
-  total_positive = len(positive_files)
-  print ('Total positive files:', total_positive)
-  total_negative = len(negative_files)
-  print ('Total negative files:', total_negative)
+  positive_patients = {}
+  negative_patients = {}
+  for patient in frames_by_patient:
+    if negative in frames_by_patient[patient][0]:
+      negative_patients[patient] = frames_by_patient[patient]
+    elif positive in frames_by_patient[patient][0]:
+      positive_patients[patient] = frames_by_patient[patient]
+  total_positive = len(positive_patients.keys())
+  print ('Total positive patients:', total_positive)
+  total_negative = len(negative_patients.keys())
+  print ('Total negative patients:', total_negative)
   #sanity check
   print('>>>>>Sanity check...')
-  print ('Expected total files:', total_files)
+  print ('Expected total patients:', len(frames_by_patient.keys()))
   print ('Total files positive+negative:', total_positive+total_negative)
 
   #calculating splits
@@ -75,221 +83,142 @@ def create_sets(path, positive, negative, model_name, model_version, model, trai
   print('Target divide perecentage:', train_test_divide)
   print('Train percentage', (float)(total_train/(total_train+test_total)))
   print('Test percentage', (float)(test_total/(total_train+test_total)))
-  print ('Expected total files::', total_files)
-  print ('Total files train+val:', total_train+test_total)
+  print ('Expected total pacients::', len(frames_by_patient.keys()))
+  print ('Total pacients train+val:', total_train+test_total)
 
   #HASTA AQUI BIEN
   
   print('*'*10)
-  print('Loading file names...')
-  print('Total positive', len(positive_files))
-  print('Total negative', len(negative_files))
+  print('Loading patients...')
+  print('Total positive', len(positive_patients))
+  print('Total negative', len(negative_patients))
   print('Expected train pos:', total_train_pos)
   print('Expected train neg:', total_train_neg)
   #train
-  train_positive_filenames = positive_files[:total_train_pos]
-  train_negative_filenames = negative_files[:total_train_neg]
-  train_files = train_positive_filenames + train_negative_filenames
+  train_positive_patients = dict(list(positive_patients.items())[:total_train_pos])
+  train_negative_patients = dict(list(negative_patients.items())[:total_train_neg])
+  train_patients = {**train_positive_patients, **train_negative_patients}
   #sanity check
   print('>>>>>Sanity check...')
   print('Expected train positive:', total_train_pos)
-  print('Actual train positive:', len(train_positive_filenames))
+  print('Actual train positive:', len(train_positive_patients))
   print('Expected train negative:', total_train_neg)
-  print('Actual train negative:', len(train_negative_filenames))
+  print('Actual train negative:', len(train_negative_patients))
   print('Expected train:', total_train)
-  print('Actual files in train_files:', len(train_files))
+  print('Actual patients in train_patients:', len(train_patients))
   #val
-  val_positive_filenames = positive_files[total_train_pos:]
-  val_negative_filenames = negative_files[total_train_neg:]
-  val_files = val_positive_filenames + val_negative_filenames
+  val_positive_patients = dict(list(positive_patients.items())[total_train_pos:])
+  val_negative_patients = dict(list(negative_patients.items())[total_train_neg:])
+  val_patients = {**val_positive_patients, **val_negative_patients}
   #sanity check
   print('>>>>>Sanity check...')
   print('Expected val positive:', test_pos)
-  print('Actual val positive:', len(val_positive_filenames))
+  print('Actual val positive:', len(val_positive_patients))
   print('Expected val negative:', test_neg)
-  print('Actual val negative:', len(val_negative_filenames))
+  print('Actual val negative:', len(val_negative_patients))
   print('Expected val:', test_total)
-  print('Actual files in val_files:', len(val_files))
-  
-  #train_files = positive_files[:total_train_pos] + negative_files[:total_train_neg]
-  #val_files = positive_files[total_train_pos:]  + negative_files[total_train_neg:]
-    
-  shuffle(train_files)
-  shuffle(val_files)
-  #loading images
-  print('Loading train and val images...')
-  # Train
-  print ('Processing training data...')
-  X_train = []
-  X_train_names = []
-  y_train = []
-  fail_train = []
-  file_processed = 0
-  for filename in train_files:
-    file_processed += 1
-    if file_processed % 300 == 0:
-      print('Processing ', file_processed, 'of', len(train_files))
-    if positive in filename: 
-      y_train.append([1,0])
-    elif negative in filename:
-      y_train.append([0,1])
-    else: #wrong filename
-      fail_train.append(filename)
-    img = image.load_img(path+filename, target_size=(224, 224, 3))
-    x = image.img_to_array(img)
-    if (model_name == "mobilenet"):
-        if (model_version == 'V1'):
-          x = preprocess_input_v1(x) #mobilenet v1
-        elif (model_version == 'V2'):
-          x = preprocess_input_v2(x) #mobilenet v2
-    X_train.append(x)
-    X_train_names.append(filename)
-  #sanity check
-  print('Sanity check...')
-  print('X_train total:', len(X_train))
-  print('y_train total:', len(y_train))
-  print('fail_train total:', len(fail_train))
-  print(fail_train)
+  print('Actual patients in val_patients:', len(val_patients))
 
-  #val
-  print ('Processing validation data...')
-  X_val = []
-  X_val_names = []
-  y_val = []
-  fail_val = []
-  file_processed = 0
-  for filename in val_files:
-    file_processed += 1
-    if file_processed % 300 == 0:
-      print('Processing ', file_processed, 'of', len(val_files))
-    if positive in filename: 
-      y_val.append([1,0])
-    elif negative in filename:
-      y_val.append([0,1])
-    else: #wrong filename
-      fail_val.append(filename)
-    img = image.load_img(path+filename, target_size=(224, 224, 3))
-    x = image.img_to_array(img)
-    if (model_name == "mobilenet"):
-        if (model_version == 'V1'):
-          x = preprocess_input_v1(x) #mobilenet v1
-        elif (model_version == 'V2'):
-          x = preprocess_input_v2(x) #mobilenet v2
-    X_val.append(x)
-    X_val_names.append(filename)
-
-  #sanity check
-  print('Sanity check...')
-  print('X_val total:', len(X_val))
-  print('y_val total:', len(y_val))
-  print('fail_val total:', len(fail_val))
-  print(fail_val)
-
-  X_train = np.array(X_train)
-  y_train = np.array(y_train)
-  X_val = np.array(X_val)
-  y_val = np.array(y_val)  
-  print('Shapes train')
-  print(X_train.shape)
-  print(y_train.shape)
-  print('Shapes val')
-  print(X_val.shape)
-  print(y_val.shape)
-  return X_train, y_train, X_train_names, X_val, y_val, X_val_names
-
-def create_sets_by_patients(path, positive, negative, model_name, model_version, model, train_test_divide):
-  files_covid= os.listdir(path)
-  total_files = len(files_covid)
-  print ('Total files in disk:', total_files)
-
-  train_files = []
-  val_files = []
-
-  for filename in files_covid:
-    if any(x in filename for x in TEST_SET_PATIENTS):
-      val_files.append(filename)
-    else:
-      train_files.append(filename)
-
-  print('Total train files:', len(train_files))
-  print('Total test files:', len(val_files))
+  print('Total train patients:', len(train_patients))
+  print('Total test patients:', len(val_patients))
  
   #loading images
   print('Loading train and val images...')
   # Train
   print ('Processing training data...')
+  patient_processed = 0
   X_train = []
   X_train_names = []
   y_train = []
   fail_train = []
-  file_processed = 0
-  for filename in train_files:
-    file_processed += 1
-    if file_processed % 300 == 0:
-      print('Processing ', file_processed, 'of', len(train_files))
-    if positive in filename: 
-      y_train.append([1,0])
-    elif negative in filename:
-      y_train.append([0,1])
-    else: #wrong filename
-      fail_train.append(filename)
-    img = image.load_img(path+filename, target_size=(224, 224, 3))
-    x = image.img_to_array(img)
-    if (model_name == "mobilenet"):
-        if (model_version == 'V1'):
-          x = preprocess_input_v1(x) #mobilenet v1
-        elif (model_version == 'V2'):
-          x = preprocess_input_v2(x) #mobilenet v2
-    X_train.append(x)
-    X_train_names.append(filename)
+  for patient in train_patients.items():
+    patient_processed += 1
+    if patient_processed % 5 == 0:
+      print('Processing ', patient_processed, 'of', len(train_patients))
+    X_train_patient = []
+    X_train_names_patient = []
+    fail_train_patient = []
+    frames_processed = 0
+    for filename in patient[1]:
+      if MAX_FRAMES <= frames_processed:
+        break
+      if frames_processed == 0:
+        if positive in filename:
+          y_train.append([1,0])
+        elif negative in filename:
+          y_train.append([0,1])
+      img = image.load_img(path+filename, target_size=(IMG_SIZE, IMG_SIZE, CHANNELS))
+      x = image.img_to_array(img)
+      if (model_name == "mobilenet"):
+          if (model_version == 'V1'):
+            x = preprocess_input_v1(x) #mobilenet v1
+          elif (model_version == 'V2'):
+            x = preprocess_input_v2(x) #mobilenet v2
+      X_train_patient.append(x)
+      X_train_names_patient.append(filename)
+      frames_processed += 1
+    while (MAX_FRAMES > frames_processed):
+      X_train_patient.append(np.zeros((IMG_SIZE, IMG_SIZE, CHANNELS)))
+      X_train_names_patient.append('padding_filename')
+      frames_processed += 1
+    X_train.append(np.array(X_train_patient))
+    X_train_names.append(np.array(X_train_names_patient))
+    fail_train.append(np.array(fail_train_patient))
+    
   #sanity check
   print('Sanity check...')
   print('X_train total:', len(X_train))
   print('y_train total:', len(y_train))
-  print('fail_train total:', len(fail_train))
+  print('fail_train:')
   print(fail_train)
   
   #val
   print ('Processing validation data...')
+  patient_processed = 0
   X_val = []
   X_val_names = []
   y_val = []
   fail_val = []
-  file_processed = 0
-  test_pos_total = 0
-  test_neg_total = 0
-  for filename in val_files:
-    file_processed += 1
-    if file_processed % 300 == 0:
-      print('Processing ', file_processed, 'of', len(val_files))
-    if positive in filename: 
-      y_val.append([1,0])
-      test_pos_total += 1
-    elif negative in filename:
-      y_val.append([0,1])
-      test_neg_total += 1
-    else: #wrong filename
-      fail_val.append(filename)
-    img = image.load_img(path+filename, target_size=(224, 224, 3))
-    x = image.img_to_array(img)
-    if (model_name == "mobilenet"):
-        if (model_version == 'V1'):
-          x = preprocess_input_v1(x) #mobilenet v1
-        elif (model_version == 'V2'):
-          x = preprocess_input_v2(x) #mobilenet v2
-    X_val.append(x)
-    X_val_names.append(filename)
+  for patient in val_patients.items():
+    patient_processed += 1
+    if patient_processed % 5 == 0:
+      print('Processing ', patient_processed, 'of', len(val_patients))
+    X_val_patient = []
+    X_val_names_patient = []
+    fail_val_patient = []
+    frames_processed = 0
+    for filename in patient[1]:
+      if MAX_FRAMES <= frames_processed:
+        break
+      if frames_processed == 0:
+        if positive in filename:
+          y_val.append([1,0])
+        elif negative in filename:
+          y_val.append([0,1])
+      img = image.load_img(path+filename, target_size=(IMG_SIZE, IMG_SIZE))
+      x = image.img_to_array(img)
+      if (model_name == "mobilenet"):
+          if (model_version == 'V1'):
+            x = preprocess_input_v1(x) #mobilenet v1
+          elif (model_version == 'V2'):
+            x = preprocess_input_v2(x) #mobilenet v2
+      X_val_patient.append(x)
+      X_val_names_patient.append(filename)
+      frames_processed += 1
+    while (MAX_FRAMES > frames_processed):
+      X_val_patient.append(np.zeros((IMG_SIZE, IMG_SIZE, CHANNELS)))
+      X_val_names_patient.append('padding_filename')
+      frames_processed += 1
+    X_val.append(np.array(X_val_patient))
+    X_val_names.append(np.array(X_val_names_patient))
+    fail_val.append(np.array(fail_val_patient))
 
   #sanity check
   print('Sanity check...')
   print('X_val total:', len(X_val))
   print('y_val total:', len(y_val))
-  print('fail_val total:', len(fail_val))
+  print('fail_val:')
   print(fail_val)
-  print('Test positive examples:', test_pos_total)
-  print((float)(test_pos_total/len(y_val)))
-  print('Test negative examples:', test_neg_total)
-  print((float)(test_neg_total/len(y_val)))
 
   X_train = np.array(X_train)
   y_train = np.array(y_train)
@@ -338,7 +267,7 @@ if __name__ == '__main__':
                         help="Epochs value between 1 and infinite")
     parser.add_argument("--batch_size",
                         type=int,
-                        default=32,
+                        default=1,
                         nargs="?",
                         help="Batch size value")
     parser.add_argument("--steps_per_epoch",
@@ -389,6 +318,11 @@ if __name__ == '__main__':
     seed(args.random_seed)
     set_seed(args.random_seed)
 
+    # images params
+    MAX_FRAMES = 200
+    IMG_SIZE = 224
+    CHANNELS = 3
+
     # get the model without the denses
     if (args.model == 'mobilenet'):
       if (args.model_version == 'V1'):
@@ -416,49 +350,66 @@ if __name__ == '__main__':
     last_layer = base_model.layers[-1]
     
     new_top_layer_global_avg_pooling = GlobalAveragePooling2D()(last_layer.output)
-    new_top_layer = Dense(2)(new_top_layer_global_avg_pooling)
-    model = Model(base_model.input, new_top_layer)
-
-    # we will only train the new denses for the baseline
+    cnn = Model(base_model.input, new_top_layer_global_avg_pooling)
+    
+    # we will only train the LSTM and the new denses
     for layer in base_model.layers:
       layer.trainable = False
+
+    # see CNN model structure
+    cnn.summary()
+
+    # input several frames for the LSTM
+    inputs = Input(shape=(MAX_FRAMES, IMG_SIZE, IMG_SIZE, CHANNELS))
+
+    # add recurrency, time distributed layers
+    encoded_frames = TimeDistributed(cnn)(inputs)
+    encoded_sequence = LSTM(256)(encoded_frames)
+
+    # classification layers
+    new_dense = Dense(1024, activation='relu')(encoded_sequence)
+    predictions = Dense(2, activation='softmax')(new_dense)
+    model = Model(inputs=[inputs], outputs=predictions)
 
     # compile model
     model.compile(optimizer=args.optimizer, loss=args.loss, metrics = ["accuracy"])
 
-    # see model structure
-    model.summary()
-
     # get the data
     print('***** Load files...')
-    if args.strategy == 'combined':
-      X_train, y_train, X_train_names, X_val, y_val, X_val_names = create_sets(args.dataset_path,
-        args.label_dataset_zero,
-        args.label_dataset_one,
-        args.model,
-        args.model_version,
-        model,
-        args.train_test_divide)
-    elif args.strategy == 'by_patients':
-      X_train, y_train, X_train_names, X_val, y_val, X_val_names = create_sets_by_patients(args.dataset_path,
-        args.label_dataset_zero,
-        args.label_dataset_one,
-        args.model,
-        args.model_version,
-        model,
-        args.train_test_divide)
-    
+    X_train, y_train, X_train_names, X_val, y_val, X_val_names = create_sets(args.dataset_path,
+      args.label_dataset_zero,
+      args.label_dataset_one,
+      args.model,
+      args.model_version,
+      IMG_SIZE,
+      MAX_FRAMES,
+      CHANNELS,
+      args.train_test_divide)
+
+    # see final model structure (CNN + LSTM)
+    model.summary()
+
+    # input shape
+    print("Input shape")
+    print(X_train.shape)
+    # input sample shape
+    print("Input sample shape")
+    print(X_train[0].shape)
+    print(X_train[1].shape)
+    print(X_train[15].shape)
+    print(X_train[42].shape)
+
     # fit model
     if (args.use_steps_per_epoch == 1):
       results = model.fit(X_train, y_train, epochs=args.epochs, steps_per_epoch=args.steps_per_epoch, batch_size=args.batch_size, validation_data=(X_val, y_val))
     else:
-      results = model.fit(X_train, y_train, epochs=args.epochs, batch_size=args.batch_size, validation_data=(X_val, y_val))
+      results = model.fit(X_train, y_train, epochs=args.epochs, steps_per_epoch=args.steps_per_epoch, batch_size=args.batch_size, validation_data=(X_val, y_val))
 
     print('#' * 40)
     print("Finished! Saving model")
 
     # save model
-    model.save(args.results_path + 'covid19_model_' 
+    model.save(args.results_path + 'covid19_model_temporal_' 
       + args.model + args.model_version + "_for_" + args.label_dataset_zero + "_" + args.label_dataset_one)
 
     print('#' * 40)
